e5e848d7f9ae9bc86c30d0ef3764fc0db35c0240
Nirmal Fernando <nirmal070125@gmail.com>
2015-03-19 09:05:24 +0530

    Changes after latest refactoring.

--- logistic-regression/model-generation.sh
+++ logistic-regression/model-generation.sh
@@ -2,7 +2,7 @@
 #@author nirmal (github: nirmal070125)
 echo "#create a dataset"
 curl -X POST -d @'create-dataset' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/datasets -k
-sleep 2
+sleep 5
 #get valueset id
 echo "#create a project"
 curl -X POST -d @'create-project' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/projects -k
@@ -17,17 +17,17 @@ sleep 2
 echo "#get analysis id"
 #curl -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/analyses/nirmal-analysis1 -k
 sleep 2
-echo "#create model"
-curl -X POST -d @'create-model' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/models -k
-sleep 2
 echo "#setting model configs"
-curl -X POST -d @'create-model-config' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/models/1/configurations -k -v
+curl -X POST -d @'create-model-config' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/analyses/1/configurations -k -v
 sleep 2
 echo "#add default features with customized options"
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/models/1/features/defaults -k -v -d @'customized-features'
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/analyses/1/features/defaults -k -v -d @'customized-features'
 sleep 2
 echo "#add default hyper params"
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/models/1/hyperParams/defaults -k -v
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/analyses/1/hyperParams/defaults -k -v
+sleep 2
+echo "#create model"
+curl -X POST -d @'create-model' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/models -k
 sleep 2
 echo "#add model storage information"
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://localhost:9443/api/models/1/storages -k -v -d @'create-model-storage'
